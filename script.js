1e03b5659f5c0839ce98b754ee071015f5f7fb53
smallptarmigan <32320750+smallptarmigan@users.noreply.github.com>
2019-08-13 20:30:21 +0900

    update js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 function OnButtonClick() {
    
-    var pass;
+    var data;
     $(document).ready(function () {
-        $.getJSON("data.json", function(data){
-            pass = data;
+        $.getJSON("https://smallptarmigan.github.io/data.json", function(d){
+            var
+                ulObj = $("#demo"),
+                len = data.length;
+
+            for(var i = 0; i < len; i++) {
+                ulObj.append($("<li>").attr({"id":data[i].id}).text(data[i].name));
+            }
         });
     });
+
+    //data = data["park_list"]
 
     var idelist = ["output1", "output2", "output3", "output4"]
     var idtlist = ["text1", "text2", "text3", "text4"]
 
     for (var i; i<idelist.length; i++){
-        var rand = Math.floor(Math.random() * (pass.length-1));
+        var rand = Math.floor(Math.random() * (data.length-1));
         target = document.getElementById(idelist[i]);
-        target.innerHTML = '<img src="park/'+pass['pic']+'" alt="park1" width="100">';
+        target.innerHTML = '<img src="park/'+data['pic']+'" alt="park1" width="100">';
         target = document.getElementById(idtlist[i]);
-        target.innerHTML = pass['name'];
+        target.innerHTML = data['name'];
     }
 }
 
