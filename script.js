80a03ec3f431ffe0be7095bb855295ea14ba00a7
smallptarmigan <32320750+smallptarmigan@users.noreply.github.com>
2019-08-14 00:45:16 +0900

    update js

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
 function OnButtonClick() {
-   
+    var arr; 
+
     $(document).ready(function () {
         $.getJSON("https://smallptarmigan.github.io/data.json", function(data){
             var arr = data["park_list"];
-      
-            //取得したJSONデータをコンソールに表示する
-            for(let i in arr){
-              console.log(arr[i]);
-            }
         });
     });
-
-    console.log("debag");
-    //data = data["park_list"]
 
     var idelist = ["output1", "output2", "output3", "output4"]
     var idtlist = ["text1", "text2", "text3", "text4"]
 
     for (var i; i<idelist.length; i++){
-        var rand = Math.floor(Math.random() * (data.length-1));
+        var rand = Math.floor(Math.random() * (arr.length-1));
         target = document.getElementById(idelist[i]);
-        target.innerHTML = '<img src="park/'+data['pic']+'" alt="park1" width="100">';
+        target.innerHTML = '<img src="https://smallptarmigan.github.io/park/'+arr['pic']+'" alt="park1" width="100">';
         target = document.getElementById(idtlist[i]);
-        target.innerHTML = data['name'];
+        target.innerHTML = arr['name'];
     }
 }
 
