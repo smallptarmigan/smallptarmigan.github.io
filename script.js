eb5752d0455215dcc889dcdfda9521d6ebea816d
smallptarmigan <32320750+smallptarmigan@users.noreply.github.com>
2019-08-13 19:30:15 +0900

    update js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function OnButtonClick() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "./data.json");
-    var pass = JSON.parse(xmlhttp.responseText || "null");
-    console.log(pass);
+   
+    var pass;
+    $(document).ready(function () {
+        $.getJSON("data.json", function(data){
+            pass = data;
+        });
+    });
 
     var idelist = ["output1", "output2", "output3", "output4"]
     var idtlist = ["text1", "text2", "text3", "text4"]
@@ -10,8 +13,9 @@ function OnButtonClick() {
     for (var i; i<idelist.length; i++){
         var rand = Math.floor(Math.random() * (pass.length-1));
         target = document.getElementById(idelist[i]);
-        target.innerHTML = '<img src="park/'+pass[rand]+'" alt="park1" width="100">';
+        target.innerHTML = '<img src="park/'+pass['pic']+'" alt="park1" width="100">';
         target = document.getElementById(idtlist[i]);
-        target.innerHTML = pass[rand];
+        target.innerHTML = pass['name'];
     }
 }
+
