b6847405e53ea7115c7fbff34a5f334dcaf767bc
smallptarmigan <32320750+smallptarmigan@users.noreply.github.com>
2019-08-14 01:14:54 +0900

    update js

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 function OnButtonClick() {
-  var arr = []; 
-
   $(document).ready(function () {
     $.getJSON("https://smallptarmigan.github.io/data.json", function(data){
-      arr = data["park_list"];
+
+      var idelist = ["output1", "output2", "output3", "output4"]
+      var idtlist = ["text1", "text2", "text3", "text4"]
+
+      var parklog = [0,0,0,0]
+
+      for (var i=0; i<idelist.length; i++){
+        var rand = Math.floor(Math.random() * (data["park_list"].length));
+        target = document.getElementById(idelist[i]);
+        target.innerHTML = '<img src="https://smallptarmigan.github.io/park/'+data["park_list"][rand]['pic']+'" alt="park1" width="100">';
+        target = document.getElementById(idtlist[i]);
+        target.innerHTML = data["park_list"][rand]['name'];
+        parklog[i] = rand
+      }
+      // console.log(parklog)
     });
   });
-
-  
-  console.log(arr)
-  
-
-  var idelist = ["output1", "output2", "output3", "output4"]
-  var idtlist = ["text1", "text2", "text3", "text4"]
-
-  for (var i; i<idelist.length; i++){
-    var rand = Math.floor(Math.random() * (arr.length));
-    target = document.getElementById(idelist[i]);
-    target.innerHTML = '<img src="https://smallptarmigan.github.io/park/'+arr['pic']+'" alt="park1" width="100">';
-    target = document.getElementById(idtlist[i]);
-    target.innerHTML = arr['name'];
-  }
-
-  console.log("click")
 }
 
