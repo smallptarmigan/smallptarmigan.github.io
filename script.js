aa59aaead93737bf50f39ae6c238dd3ad513f951
smallptarmigan <32320750+smallptarmigan@users.noreply.github.com>
2019-08-14 01:19:41 +0900

    add semicoron

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ function OnButtonClick() {
   $(document).ready(function () {
     $.getJSON("https://smallptarmigan.github.io/data.json", function(data){
 
-      var idelist = ["output1", "output2", "output3", "output4"]
-      var idtlist = ["text1", "text2", "text3", "text4"]
+      var idelist = ["output1", "output2", "output3", "output4"];
+      var idtlist = ["text1", "text2", "text3", "text4"];
 
-      var parklog = [0,0,0,0]
+      var parklog = [0,0,0,0];
 
       for (var i=0; i<idelist.length; i++){
         var rand = Math.floor(Math.random() * (data["park_list"].length));
-        target = document.getElementById(idelist[i]);
-        target.innerHTML = '<img src="https://smallptarmigan.github.io/park/'+data["park_list"][rand]['pic']+'" alt="park1" width="100">';
         target = document.getElementById(idtlist[i]);
         target.innerHTML = data["park_list"][rand]['name'];
-        parklog[i] = rand
+        target = document.getElementById(idelist[i]);
+        target.innerHTML = '<img src="https://smallptarmigan.github.io/park/'+data["park_list"][rand]['pic']+'" alt="park" width="100">';
+        parklog[i] = rand;
       }
-      // console.log(parklog)
+      // console.log(parklog);
     });
   });
 }
