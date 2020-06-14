6eb752f72c13ccf9c1f453e770fc79c5fbe18ef1
smallptarmigan <32320750+smallptarmigan@users.noreply.github.com>
2020-06-14 17:49:57 +0900

    addition of board drawing

--- othello/src/main.js
+++ othello/src/main.js
@@ -3,8 +3,9 @@ var screenCanvas, info;
 var run = true;
 var fps = 1000 / 30;
 var mouse = new Point();
-var ctx;
+var ctx_cell, ctx_board;
 var turn = false;
+var boarddata = new Array(8*8);
 
 // - const --------------------------------------------------------------------
 var PIECE_BLACK_COLOR = 'rgba(0, 0, 0, 1)';
@@ -30,7 +31,7 @@ window.onload = function(){
 
     // エレメント関連
     info = document.getElementById('info');
-
+    
     // ループ処理を呼び出す
     (function(){
         // HTMLを更新
@@ -38,18 +39,41 @@ window.onload = function(){
 
         // screenクリア 
         ctx.clearRect(0, 0, screenCanvas.width, screenCanvas.height);
+        
+        // パスの設定を開始
+        ctx.beginPath();
+        // 盤面の色を設定する
+        ctx.fillStyle = 'rgba(0, 170, 0, 1.0)';
+        // 四角を描くパスを設定
+        ctx.rect(0, 0, screenCanvas.width, screenCanvas.height);
+        // 四角を描く
+        ctx.fill();
 
         // パスの設定を開始
         ctx.beginPath();
+        // 枠の色を設定する
+        ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
+        // 枠線を描くパスを設定
+        for(let i=1; i<8; i++){
+            ctx.moveTo(i*wakusize, 0);
+            ctx.lineTo(i*wakusize, screenCanvas.height)
+        }
+        for(let i=1; i<8; i++){
+            ctx.moveTo(0, i*wakusize);
+            ctx.lineTo(screenCanvas.width, i*wakusize)
+        }
+        // 線を描く
+        ctx.closePath();
+        ctx.stroke();
 
+        // パスの設定を開始
+        ctx.beginPath();
         // 選択マスの色を設定する
         ctx.fillStyle = 'rgba(255, 255, 0, 0.75)';
-
         // 四角を描くパスを設定
         cellx = mouse.x / wakusize | 0;
         celly = mouse.y / wakusize | 0;
         ctx.rect(cellx*wakusize, celly*wakusize, wakusize, wakusize);
-
         // 四角を描く
         ctx.fill();
 
